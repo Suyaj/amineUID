7fa31d66fb6144ba11461d9cbef48fbba11110b3
Suyaj <2739618360@qq.com>
2024-12-02 02:52:40 +0800

    初始化

--- amineUID/utils/__init__.py
+++ amineUID/utils/__init__.py
@@ -1,1 +1,7 @@
 """ init """
+import os
+
+from gsuid_core.plugins.amineUID.amineUID.utils.contants import MAIN_PATH, IMAGES_PATH
+
+os.makedirs(MAIN_PATH, exist_ok=True)
+os.makedirs(IMAGES_PATH, exist_ok=True)
