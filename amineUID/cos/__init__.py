9ff765423cb4739dffca763e0019401ee4202849
YYW <yywang@che300.com>
2024-12-02 14:06:34 +0800

    Message 包装

--- amineUID/cos/__init__.py
+++ amineUID/cos/__init__.py
@@ -60,5 +60,5 @@ async def get_cos_images(bot: Bot, ev: Event):
     image_open = Image.open(
         os.path.join(img_dir, "0c932ae2f40e687a7ae5fa5ab119609a.jpg"))
     img = await convert_img(image_open)
-    msgs = [img, img]
+    msgs = [Message(img), Message(img)]
     await bot.send(msgs)
