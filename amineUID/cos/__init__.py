61e2a8fc2ebd7d3d3b7c004343a7ef1e34633a30
YYW <yywang@che300.com>
2024-12-03 13:35:46 +0800

    ✨添加新功能

--- amineUID/cos/__init__.py
+++ amineUID/cos/__init__.py
@@ -32,14 +32,6 @@ async def get_cos_images(bot: Bot, ev: Event):
     ev_text = ev.text.strip()
     cmds = convert_cmd(ev_text)
     msg = ""
-    if len(cmds) == 0:
-        data = os.listdir(IMAGES_PATH)
-        if len(data) == 0:
-            return bot.send("本地无数据")
-        i = random.randint(0, len(data) - 1)
-        img_dir = os.path.join(IMAGES_PATH, data[i])
-        images = await get_images(Path(img_dir))
-        return bot.send(images)
     await bot.send('正在获取目录，请稍等')
     if "local" in cmds:
         data = os.listdir(IMAGES_PATH)
@@ -91,6 +83,15 @@ async def get_cos_images(bot: Bot, ev: Event):
             logger.error(e)
             await bot.send('输入错误')
 
+@sv_am_get_cos_images.on_fullmatch("获取图片")
+async def get_cos_images(bot: Bot, ev: Event):
+    data = os.listdir(IMAGES_PATH)
+    if len(data) == 0:
+        return bot.send("本地无数据")
+    i = random.randint(0, len(data) - 1)
+    img_dir = os.path.join(IMAGES_PATH, data[i])
+    images = await get_images(Path(img_dir))
+    return bot.send(images)
 
 def convert_cmd(text: str) -> dict[str, str]:
     cmd = {}
