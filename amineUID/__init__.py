89f331a070412d1118b5ffed18d17dcd59e2374e
YYW <yywang@che300.com>
2025-02-07 17:57:09 +0800

    🔔添加签到

--- amineUID/__init__.py
+++ amineUID/__init__.py
@@ -2,4 +2,4 @@
 
 from gsuid_core.sv import Plugins
 
-Plugins(name="amineUID", force_prefix=['am', 'AM'], allow_empty_prefix=False)
+Plugins(name="amineUID", force_prefix=['am', 'AM', 'wiki'], allow_empty_prefix=False)
---
+++ amineUID/wiki/__init__.py
@@ -0,0 +1,32 @@
+from gsuid_core.plugins.WutheringWavesUID.WutheringWavesUID.wutheringwaves_newsign import do_sign_task
+from gsuid_core.plugins.ZZZeroUID.ZZZeroUID.utils.hint import BIND_UID_HINT
+from gsuid_core.sv import SV
+from gsuid_core.bot import Bot
+from gsuid_core.models import Event
+from gsuid_core.logger import logger
+from gsuid_core.utils.database.models import GsBind
+from gsuid_core.utils.sign.sign import sign_in
+
+sv_wiki = SV("wiki")
+
+@sv_wiki.on_fullmatch("全部签到")
+async def get_all_sign_func(bot: Bot, ev: Event):
+    logger.info(f'[全部签到] 用户: {ev.user_id}')
+    uid = await GsBind.get_uid_by_game(ev.user_id, ev.bot_id, 'gs')
+    if uid is None:
+        return await bot.send(BIND_UID_HINT)
+    # gs签到
+    await bot.send(await sign_in(uid, 'gs'))
+    # sr签到
+    uid = await GsBind.get_uid_by_game(ev.user_id, ev.bot_id, 'sr')
+    if uid is None:
+        return await bot.send(BIND_UID_HINT)
+    await bot.send(await sign_in(uid, 'sr'))
+    # zzz签到
+    uid = await GsBind.get_uid_by_game(ev.user_id, ev.bot_id, 'zzz')
+    if uid is None:
+        return await bot.send(BIND_UID_HINT)
+    await bot.send(await sign_in(uid, 'zzz'))
+    # ww签到
+    msg = await do_sign_task(bot, ev)
+    await bot.send(msg)
