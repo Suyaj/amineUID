fd03d2f25974ae6ee1e4b9adc6b615ea03c28a64
Suyaj <2739618360@qq.com>
2025-03-18 22:49:38 +0800

    ✨修改签到方法

--- amineUID/wiki/__init__.py
+++ amineUID/wiki/__init__.py
@@ -1,7 +1,5 @@
-from webdriver_manager.chrome import ChromeDriverManager
-
 from gsuid_core.plugins.amineUID.amineUID.wiki.gs_wiki import get_future
-from gsuid_core.plugins.WutheringWavesUID.WutheringWavesUID.wutheringwaves_newsign import do_sign_task
+from gsuid_core.plugins.WutheringWavesUID.WutheringWavesUID.wutheringwaves_newsign import get_sign_func
 from gsuid_core.plugins.ZZZeroUID.ZZZeroUID.utils.hint import BIND_UID_HINT
 from gsuid_core.sv import SV
 from gsuid_core.bot import Bot
@@ -32,7 +30,7 @@ async def get_all_sign_func(bot: Bot, ev: Event):
         return await bot.send(BIND_UID_HINT)
     await bot.send(await sign_in(uid, 'zzz'))
     # ww签到
-    msg = await do_sign_task(bot, ev)
+    msg = await get_sign_func(bot, ev)
     await bot.send(msg)
 
 
