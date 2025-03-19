7d3cf6528d3d2fde937c5cfb8245ded1d7bde290
Suyaj <2739618360@qq.com>
2025-03-19 21:46:08 +0800

    ✨测试

--- amineUID/wiki/driver_test.py
+++ amineUID/wiki/driver_test.py
@@ -1,51 +1,45 @@
+import asyncio
 import base64
 import time
 from io import BytesIO
 
 from PIL import Image
+from playwright.async_api import async_playwright
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
+async def test():
+    playwright = await async_playwright().start()
+    launch = await playwright.chromium.launch(headless=True)
+    page = await launch.new_page()
+    await page.goto("https://homdgcat.wiki")
+    await page.wait_for_function("()=>{return document.getElementsByClassName('n1').length > 0;}")
+    await page.wait_for_function('''
+    () => {
+        let images = document.getElementsByClassName("n1")[0].getElementsByTagName('img');
+        for (let image of images) {
+            if(!image.complete){
+              return false;
+            }
+        }
+        return true;
+    }
+    ''')
+    node = await page.query_selector("body > container > div > section.n1")
+    binary_data = await node.screenshot()
+    elements = await node.query_selector_all("a")
+    size = len(elements)
+    box = await elements[0].bounding_box()
+    _width = box['width']
+    img_width = (_width + 8) * size
+    image_data = BytesIO(binary_data)
+    img = Image.open(image_data)
+    height = img.size[1]
+    im = img.crop((0, 0, img_width, height))
+    im.save('test.png')
+
 if __name__ == '__main__':
-    option = webdriver.ChromeOptions()
-    option.add_argument('--headless')
-    option.add_argument('--no-sandbox')
-    option.add_argument('--disable-gpu')
-    option.add_argument('--disable-dev-shadow')
-    option.add_argument('--allow-system-access')
-    service = Service(executable_path = '/snap/bin/chromium.chromedriver')
-    driver = webdriver.Chrome(options=option, service=service)
-    try:
-        driver.get("https://homdgcat.wiki")
-        WebDriverWait(driver, 60).until(
-            ec.presence_of_element_located((By.XPATH, '/html/body/container/div/section[4]'))
-        )
-        ready_state = driver.execute_script(
-            "let images = document.getElementsByClassName('n1')[0].getElementsByTagName('img');for (let image of images) {  if(!image.complete){    return false;  }}return true;")
-        while not ready_state:
-            ready_state = driver.execute_script(
-                "let images = document.getElementsByClassName('n1')[0].getElementsByTagName('img');for (let image of images) {  if(!image.complete){    return false;  }}return true;")
-        time.sleep(1)
-        html = driver.find_element(By.CSS_SELECTOR, 'container')
-        # 将浏览器的宽高设置最大
-        driver.set_window_size(html.size['width'], html.size['height'])
-        time.sleep(1)
-        r_node = driver.find_element(By.XPATH, value='/html/body/container/div/section[4]')
-        _base64 = r_node.screenshot_as_base64
-        elements = r_node.find_elements(By.CSS_SELECTOR, 'a')
-        size = len(elements)
-        _width = elements[0].size['width']
-        img_width = (_width + 8) * size
-        binary_data = base64.b64decode(_base64)
-        image_data = BytesIO(binary_data)
-        img = Image.open(image_data)
-        height = img.size[1]
-        im = img.crop((0, 0, img_width, height))
-        im.save("result.png")
-    except Exception as e:
-        print('请求错误:{}', e)
-    finally:
-        driver.quit()
+    asyncio.run(test())
