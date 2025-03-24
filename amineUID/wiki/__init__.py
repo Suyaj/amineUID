b7fccc9ae9f9e018dc2ccecc1558bc39c7cc78b6
YYW <yywang@che300.com>
2025-03-24 14:11:18 +0800

    🔔修改命令类型

--- amineUID/wiki/__init__.py
+++ amineUID/wiki/__init__.py
@@ -67,7 +67,7 @@ async def get_future_func(bot: Bot, ev: Event):
     await bot.send(image)
 
 
-@sv_wiki.on_fullmatch('刷新未来信息')
+@sv_wiki.on_prefix('刷新未来信息')
 async def get_refresh_data(bot: Bot, ev: Event):
     await bot.send("正在启动刷新程序，请稍后！！！")
     texts = ev.text.strip().split(" ")
