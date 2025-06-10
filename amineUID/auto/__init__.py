4594c80dda8cf9c3f2d8cb437db9cbfda25c94ee
YYW <yywang@che300.com>
2025-06-10 16:41:39 +0800

    🔔签到

--- amineUID/auto/__init__.py
+++ amineUID/auto/__init__.py
@@ -4,9 +4,10 @@ from gsuid_core.aps import scheduler
 from gsuid_core.logger import logger
 from gsuid_core.config import core_config
 from gsuid_core.utils.database.models import GsBind
+import gsuid_core.utils.sign.sign as sign
 
 
-@scheduler.scheduled_job('cron', hour=16, minute=35)
+@scheduler.scheduled_job('cron', hour=16, minute=43)
 async def sign_in():
     logger.info("开始签到")
     masters = core_config.get_config('masters')
@@ -17,21 +18,21 @@ async def sign_in():
             msg_list.append(f"[{master}]你还没有绑定UID哦, 请使用原神绑定uid 完成绑定！")
         else:
             # gs签到
-            msg = await sign_in(uid, 'gs')
+            msg = await sign.sign_in(uid, 'gs')
             msg_list.append(msg)
         # sr签到
         uid = await GsBind.get_uid_by_game(master, "onebot", 'sr')
         if uid is None:
             msg_list.append(f"[{master}]你还没有绑定UID哦, 请使用崩铁绑定uid 完成绑定！")
         else:
-            msg = await sign_in(uid, 'sr')
+            msg = await sign.sign_in(uid, 'sr')
             msg_list.append(msg)
         # zzz签到
         uid = await GsBind.get_uid_by_game(master, "onebot", 'zzz')
         if uid is None:
             msg_list.append(f"[{master}]{BIND_UID_HINT}")
         else:
-            msg = await sign_in(uid, 'zzz')
+            msg = await sign.sign_in(uid, 'zzz')
             msg_list.append(msg)
         http_bot.send_private_msg(master, msg_list)
     logger.info("签到结束")
