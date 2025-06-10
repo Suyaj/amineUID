f1025ef5f66f501b3efbe2d7515202ec31dd374e
YYW <yywang@che300.com>
2025-06-10 16:25:38 +0800

    🔔签到

--- amineUID/auto/__init__.py
+++ amineUID/auto/__init__.py
@@ -12,7 +12,7 @@ async def sign_in():
     masters = core_config.get_config('masters')
     for master in masters:
         msg_list = []
-        uid = await GsBind.get_uid_by_game(master, "3490728073")
+        uid = await GsBind.get_uid_by_game(master, "onebot")
         if uid is None:
             msg_list.append(f"[{master}]你还没有绑定UID哦, 请使用原神绑定uid 完成绑定！")
         else:
@@ -20,14 +20,14 @@ async def sign_in():
             msg = await sign_in(uid, 'gs')
             msg_list.append(msg)
         # sr签到
-        uid = await GsBind.get_uid_by_game(master, "3490728073", 'sr')
+        uid = await GsBind.get_uid_by_game(master, "onebot", 'sr')
         if uid is None:
             msg_list.append(f"[{master}]你还没有绑定UID哦, 请使用崩铁绑定uid 完成绑定！")
         else:
             msg = await sign_in(uid, 'sr')
             msg_list.append(msg)
         # zzz签到
-        uid = await GsBind.get_uid_by_game(master, "3490728073", 'zzz')
+        uid = await GsBind.get_uid_by_game(master, "onebot", 'zzz')
         if uid is None:
             msg_list.append(f"[{master}]{BIND_UID_HINT}")
         else:
